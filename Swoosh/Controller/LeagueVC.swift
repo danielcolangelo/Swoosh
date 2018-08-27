0f15e3e45e49c90a4aa565c8a3722da7f1c2a894
danielcolangelo <danalfons@yahoo.com>
2018-08-26 23:50:47 -0400

    added logic for programatic segues

--- Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Controller/LeagueVC.swift
@@ -9,16 +9,42 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player()
 
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        
+        
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+        nextBtn.setTitleColor(.white, for: .normal)
+        
     }
     
 
---
+++ Swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Daniel Colangelo on 8/26/18.
+//  Copyright © 2018 Daniel Colangelo. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
